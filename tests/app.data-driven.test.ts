90a948c1e5345d568ee479dd9150ee1d49d113b7
Xiaopeng Yue <xiaopengyue.adam@gmail.com>
2024-09-23 09:33:17 +1000

    addd more tests

--- tests/app.data-driven.test.ts
+++ tests/app.data-driven.test.ts
@@ -2,9 +2,11 @@ import CommandManager from "commands/command-manager"
 import Robot from "models/robot"
 import Table from "models/table"
 import { Logger } from "utils/logger"
+import { ERRORS } from "consts/messages"
+
 import successCases from "./data/success.json"
 import invalidRobotCases from "./data/invalid-robot.json" // no valid commands before the final REPORT
-import { ERRORS } from "consts/messages"
+import invalidCommandCases from "./data/invalid-command.json" // random input
 
 jest.mock("utils/logger")
 
@@ -34,15 +36,28 @@ describe("Robot Data-Driven Testing", () => {
     })
   })
 
-  // Testing failed cases
+  // Testing report failed cases
   describe("Report when there's no valid robot Cases", () => {
     invalidRobotCases.forEach((commands, index) => {
-      test(`log invalid error case ${index + 1}`, () => {
+      test(`log invalid robot case ${index + 1}`, () => {
         commands.forEach((command) => {
           const commandInstance = commandManager.getCommand(command)
           commandInstance.execute()
         })
         expect(errorSpy).toHaveBeenCalledWith(ERRORS.INVALID_ROBOT)
+      })
+    })
+  })
+
+  // Testing wrong input cases
+  describe("Handle invalid user input", () => {
+    invalidCommandCases.forEach((commands, index) => {
+      test(`log invalid command case ${index + 1}`, () => {
+        commands.forEach((command) => {
+          const commandInstance = commandManager.getCommand(command)
+          commandInstance.execute()
+        })
+        expect(errorSpy).toHaveBeenCalledWith(ERRORS.INVALID_COMMAND)
       })
     })
   })
